72de95a4f74b32623eb8814028404825632375ca
AnastasiiaSergeeva <73837165+AnastasiiaSergeeva@users.noreply.github.com>
2021-04-30 21:59:27 +0300

    add saved theme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
+import menuCardsTpl from   './templates/menu-cards.hbs';
+import menuCard from './menu.json';
 import './sass/main.scss';
+
+const refs = {
+     body: document.querySelector('body'),
+     menuItems : document.querySelector('.js-menu'),
+     themeSwitch: document.querySelector('.theme-switch__toggle'),
+   
+}
+
+refs.menuItems.insertAdjacentHTML('beforeend', menuCardsTpl(menuCard));
+
+
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeSwitcher = function() {
+  if (refs.themeSwitch.checked) {
+    refs.body.classList.remove(Theme.LIGHT);
+    refs.body.classList.add(Theme.DARK);
+    localStorage.setItem('Theme', Theme.DARK);
+    return;
+  }
+  refs.body.classList.remove(Theme.DARK);
+  refs.body.classList.add(Theme.LIGHT);
+  localStorage.setItem('Theme', Theme.LIGHT);
+};
+
+const savedTheme = function() {
+  if (localStorage.getItem('Theme') === 'dark-theme') {
+    refs.body.classList.add(Theme.DARK);
+    refs.themeSwitch.checked = true;
+  }
+};
+
+refs.themeSwitch.addEventListener('click', themeSwitcher);
+savedTheme();
